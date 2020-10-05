4018b74632084bcb4a661d2eae16e5a55c300311
leo-leao <leonardoleao486@gmail.com>
2020-10-05 10:16:20 -0300

    Criptografia das chaves de acesso

--- main.py
+++ main.py
@@ -7,11 +7,16 @@
 # Libraries
 import time
 from fileMonitor import FileMonitor
+from cryptography.fernet import Fernet
+
+# Open the credentials
+cred = open("credentials.txt").read()
+decode = Fernet(cred[132:176])
 
 # Connection FTP attributes
 host = "13.94.133.22"
-user = "MTI"
-password = ""
+user = decode.decrypt(str.encode(cred[264:364])).decode()
+password = decode.decrypt(str.encode(cred[539:639])).decode()
 filename = "MTI.csv"
 
 # Instantiates a monitoring object
