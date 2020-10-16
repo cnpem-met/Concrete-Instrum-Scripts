8ec1f8a81304e043b6c02a2082e821b659509b2a
leo-leao <leonardoleao486@gmail.com>
2020-10-16 14:49:39 -0300

    Finalizada a conversao dos dados

--- csvTreatment.py
+++ csvTreatment.py
@@ -4,8 +4,9 @@
     @title: CSV functions
 """
 # Libraries
-import pandas as pd
 import threading
+import pandas as pd
+from calibration import Calibration as cal
 
 class CsvTreatment(threading.Thread):
     
@@ -34,32 +35,33 @@ class CsvTreatment(threading.Thread):
             "Temperature": mux[3]} 
         # Scroll the channels and set the info into the dictionary
         for i in range(4, len(mux)):
-            if i % 2 == 0:
-                muxDictionary["Ch%d%s" % (channel, "A")] = mux[i]
-            else:
-                muxDictionary["Ch%d%s" % (channel, "B")] = mux[i]
-                channel += 1
-        print(muxDictionary)
+            if str(mux[i]) != "nan":
+                if i % 2 == 0:
+                    option = "Ch%d%s" % (channel, "A")
+                    muxDictionary[option] = cal.convertChannelA(mux[0], channel, mux[i])
+                else:
+                    # Add to dictionary with convertion to Celsius degrees
+                    muxDictionary["Ch%d%s" % (channel, "B")] = cal.convertChannelB(mux[i])
+                    channel += 1
+        muxDictionary["Number of channels"] = channel - 1
+        return muxDictionary
             
     
     # Get the last line of csv and separate data into a dictionary
     def separateLastData(self, rawData):
-        mux = []
-        setId = 0
-        tableLine = rawData.tail(1).values[0] 
+        setId = 0; mux = []; muxes = {}
+        tableLine = rawData.tail(1).values[0]
         # Scroll the vector looking for a new mux
         for i in range(len(tableLine) - 1):
             if ":" in str(tableLine[i]): # Identify a datetime cell
                 if setId != 0:
-                    self.newMux(mux)
-                mux = []; setId += 1
-                mux.append(setId); mux.append(tableLine[i])
-                print("Action: reading a new mux [%s]" % str(tableLine[i]))
-            else:
+                    muxes[cal.MUXactivated[setId - 1]] = self.newMux(mux)
+                    print("Action: adding a new mux [%d]" % (cal.MUXactivated[setId - 1]))
+                mux = []; mux.append(cal.MUXactivated[setId])
                 mux.append(tableLine[i])
-                
-            
-        self.newMux(tableLine)
+                setId += 1
+            elif tableLine[i] != "":
+                mux.append(tableLine[i])
         
     def run(self):
         while not self.kill.is_set():
