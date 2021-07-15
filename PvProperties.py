4f4f10c5eae5f6e463085cc64edcb0f6127f8a76
Leonardo Leão <leonardoleao486@gmail.com>
2021-07-15 12:35:01 -0300

    Edição das variáveis EPICS

--- PvProperties.py
+++ PvProperties.py
@@ -13,77 +13,59 @@ class PvProperties():
     file = pd.read_excel("pvs.xlsx")
     
     @staticmethod
-    def appendPvdb(pvdb, tipo, placa, posicao, nivel, canal, sensor):
+    def appendPvdb(pvdb, local, setor, posicao, nivel, orientacao, sensor):
         if(str(nivel) != "nan"):
-            if sensor == "PT100":
-                pvdb["%s:%s:TEMP:%s%s:RTD" % (tipo, placa, posicao, nivel)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-            elif sensor == "VWS2100":
-                pvdb["%s:%s:STRAIN:%s%s" % (tipo, placa, posicao, nivel)] = {'prec': 3, 'scan': 1, 'unit': 'uE'}
-                pvdb["%s:%s:TEMP:%s%s:NTC" % (tipo, placa, posicao, nivel)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+            if(sensor in ["PT100", "VWTS6000"]):
+                pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}{sensor[0]}:Temp-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+                if(sensor == "VWTS6000"):
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}N:Temp-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+                else:
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}:Strain{orientacao}-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'uE'}
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}:N:Temp-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'C'}
             else:
-                pvdb["%s:%s:TEMP:%s%s:VW" % (tipo, placa, posicao, nivel)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-                pvdb["%s:%s:TEMP:%s%s:NTC" % (tipo, placa, posicao, nivel)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-        else:
-            if sensor == "PT100":
-                pvdb["%s:%s:TEMP:%s:RTD:%s" % (tipo, placa, posicao, canal)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-            elif sensor == "VWS2100":
-                pvdb["%s:%s:STRAIN:%s:%s" % (tipo, placa, posicao, canal)] = {'prec': 3, 'scan': 1, 'unit': 'uE'}
-                pvdb["%s:%s:TEMP:%s:NTC:%s" % (tipo, placa, posicao, canal)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-            else:
-                pvdb["%s:%s:TEMP:%s:VW:%s" % (tipo, placa, posicao, canal)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
-                pvdb["%s:%s:TEMP:%s:NTC:%s" % (tipo, placa, posicao, canal)] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+                if(sensor in ["PT100", "VWTS6000"]):
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}{sensor[0]}:Temp-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+                    if(sensor == "VWTS6000"):
+                        pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}N:Temp-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'C'}
+                else:
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}:Strain-Mon"] = {'prec': 3, 'scan': 1, 'unit': 'uE'}
+                    pvdb[f"TU-{setor}{local}:SS-Concrete-{posicao}N:Temp-Mon"] ={'prec': 3, 'scan': 1, 'unit': 'C'}
         return pvdb
     
     @staticmethod
     def pvName(mux, canal, ch):
         file = PvProperties.file
-        linha = file.loc[((file["mux"] == mux) & (file["canal"] == canal))]
+        linha = file.loc[((file["Mux"] == mux) & (file["Canal"] == canal))]
+        
         if not linha.empty:
-            sensor = cal.muxHeader["mux%d" % mux][canal-1]
-            tipo = linha["tipo"].values[0]
-            placa = linha["placa"].values[0]
-            posicao = linha["posicao"].values[0]
-            nivel = linha["nivel"].values[0]
+            sensor     = cal.muxHeader["mux%d" % mux][canal-1]
+            local      = str(linha["Local"].values[0])
+            setor      = int(linha["Setor"].values[0])
+            posicao    = str(linha["Posição"].values[0]).replace("L", "").replace("P", "")
+            nivel      = str(linha["Nível"].values[0])
+            orientacao = str(linha["Orientação"].values[0])
             
-            if(str(nivel) != "nan"):
-                if sensor == "PT100":
-                    return ("%s:%s:TEMP:%s%s:RTD" % (tipo, placa, posicao, nivel))
-                elif sensor == "VWS2100":
-                    if ch == "A":
-                        return ("%s:%s:STRAIN:%s%s" % (tipo, placa, posicao, nivel))
-                    else:
-                        return ("%s:%s:TEMP:%s%s:NTC" % (tipo, placa, posicao, nivel))
-                else:
-                    if ch == "A":
-                        return ("%s:%s:TEMP:%s%s:VW" % (tipo, placa, posicao, nivel))
-                    else:
-                        return ("%s:%s:TEMP:%s%s:NTC" % (tipo, placa, posicao, nivel))
-            else:
-                if sensor == "PT100":
-                    return ("%s:%s:TEMP:%s:RTD:%s" % (tipo, placa, posicao, canal))
-                elif sensor == "VWS2100":
-                    if ch == "A":
-                        return ("%s:%s:STRAIN:%s:%s" % (tipo, placa, posicao, canal))
-                    else:
-                        return ("%s:%s:TEMP:%s:NTC:%s" % (tipo, placa, posicao, canal))
-                else:
-                    if ch == "A":
-                        return ("%s:%s:TEMP:%s:VW:%s" % (tipo, placa, posicao, canal))
-                    else:
-                        return ("%s:%s:TEMP:%s:NTC:%s" % (tipo, placa, posicao, canal))
-        else:
-            return "Dis."
+            if((sensor == "PT100" or sensor == "VWTS6000") and ch == "A"):
+                return (f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}{sensor[0]}:Temp-Mon")
+            if(sensor == "VWTS6000" and ch == "B"):
+                return (f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}N:Temp-Mon")
+            if(sensor == "VWS2100" and ch == "A"):
+                return (f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}:Strain{orientacao}-Mon")
+            if(sensor == "VWS2100" and ch == "B"):
+                return (f"TU-{setor}{local}:SS-Concrete-{posicao}{nivel}N:Temp-Mon")
 
     @staticmethod
     def pvdb():
         pvdb = {}
         for i in range((PvProperties.file.shape)[0]):
-            tipo = PvProperties.file["tipo"][i]
-            placa = PvProperties.file["placa"][i]
-            posicao = PvProperties.file["posicao"][i]
-            nivel = PvProperties.file["nivel"][i]
-            mux = PvProperties.file["mux"][i]
-            canal = PvProperties.file["canal"][i]
-            sensor = cal.muxHeader["mux%d" % mux][canal-1]
-            pvdb = PvProperties.appendPvdb(pvdb, tipo, placa, posicao, nivel, canal, sensor)
+            local      = PvProperties.file["Local"][i]
+            setor      = PvProperties.file["Setor"][i]
+            posicao    = PvProperties.file["Posição"][i]
+            nivel      = PvProperties.file["Nível"][i]
+            orientacao = PvProperties.file["Orientação"][i]
+            mux        = PvProperties.file["Mux"][i]
+            canal      = PvProperties.file["Canal"][i]
+            sensor     = cal.muxHeader["mux%d" % mux][canal-1]
+            setor      = ("0" + str(setor)) if setor < 10 else str(setor)
+            pvdb       = PvProperties.appendPvdb(pvdb, local, setor, posicao, nivel, orientacao, sensor)
         return pvdb
